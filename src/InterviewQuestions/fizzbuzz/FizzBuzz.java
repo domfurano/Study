77116ba170985c72fdb73b8811150f6898b71861
dominic <dominicfurano@gmail.com>
2019-05-09 08:00:39 -0600

    updating fizz buzz

--- src/InterviewQuestions/fizzbuzz/FizzBuzz.java
+++ src/InterviewQuestions/fizzbuzz/FizzBuzz.java
@@ -4,7 +4,7 @@ import java.util.stream.IntStream;
 
 public class FizzBuzz {
     public static void main(String[] args) {
-        fizzBuzz();
+        fizzBuzzLessComparisons();
     }
 
     private static void fizzBuzz() {
@@ -13,11 +13,11 @@ public class FizzBuzz {
             boolean divisibleBy5 = i % 5 == 0;
             if (divisibleBy3) {
                 System.out.print("Fizz");
-            } 
+            }
             if (divisibleBy5) {
                 System.out.print("Buzz");
-            } 
-            if (!divisibleBy3 && !divisibleBy5) {
+            }
+            if (!(divisibleBy3 || divisibleBy5)) {
                 System.out.print(i);
             }
             System.out.println();
@@ -34,10 +34,28 @@ public class FizzBuzz {
             if (divisibleBy5) {
                 System.out.print("Buzz");
             }
-            if (!divisibleBy3 && !divisibleBy5) {
+            if (!(divisibleBy3 || divisibleBy5)) {
                 System.out.print(i);
             }
             System.out.println();
         });
+    }
+
+    private static void fizzBuzzLessComparisons() {
+        for (int i = 1; i < 101; i++) {
+            boolean divisibleBy3 = i % 3 == 0;
+            boolean divisibleBy5 = i % 5 == 0;
+            if (!(divisibleBy3 || divisibleBy5)) {
+                System.out.println(i);
+                continue;
+            }
+            if (divisibleBy3) {
+                System.out.print("Fizz");
+            }
+            if (divisibleBy5) {
+                System.out.print("Buzz");
+            }
+            System.out.println();
+        }
     }
 }
